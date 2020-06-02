df78094b57aba0a6bcf1f8c53d192caeaa3eeb41
Glitch (material-defiant-foxtrot) <none>
2020-06-02 00:47:04 +0000

    🌖🐥 Checkpoint ./commands/invite.js:1905663/63

--- commands/invite.js
+++ commands/invite.js
@@ -4,7 +4,7 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 
     const embed = new Discord.RichEmbed()
-     .addField("<:pandaderp:558845099220402186> Link directo. <3", "[🔗Click Aquí.](https://discordapp.com/oauth2/authorize?client_id=495758665391800321&scope=bot&permissions=66583880)")
+     .addField("<a:pandahype:717173386568138772> Link directo. <3", "[🔗Click Aquí.](https://discordapp.com/oauth2/authorize?client_id=495758665391800321&scope=bot&permissions=66583880)")
      .setColor("#15f153")
     
     message.channel.send({embed});
