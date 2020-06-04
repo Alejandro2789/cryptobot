d2e83d3061c875af338ee3e1987f0b8d16d929b0
Glitch (material-defiant-foxtrot) <none>
2020-06-04 23:13:28 +0000

    🐑🌯 Checkpoint ./commands/plantilla.js:1905663/760

--- commands/plantilla.js
+++ commands/plantilla.js
@@ -3,10 +3,11 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-
-
+const embed = new Discord.RichEmbed()
+.setTitle("Reglas · Crypto Support")
+.setDescription("1. Respetar a todos los usuarios del servidor. No se permite ningún tipo de racismo.\n2. Utilizar de manera correcta cada canal. Todos los canales tienen su respectivo uso, de no darle un buen uso recibirá una advertencia.\n3. No generar discusiones en el servidor, problemas entre usuarios llevarlos a privado.\n4. No se permiten multicuentas en el servidor.\n5. El spam y [flood no están permitidos en el servidor.")
 }
 module.exports.help = {
-  name: "plantilla",
+  name: "reglas",
   aliases: []
 }
