b0c85dc831de263eeab5774de113d85a35150ddc
Glitch (material-defiant-foxtrot) <none>
2020-06-05 19:25:52 +0000

    🏃🏢 Checkpoint ./commands/error.js:1905663/316 ./commands/server.js:1905663/258 ./commands/reset-command.js:1905663/58

--- commands/error.js
+++ commands/error.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js");
-
+const emoji = require("../emojis.json");
 
 module.exports.run = async (bot, message, args) => {
 
@@ -7,8 +7,8 @@ module.exports.run = async (bot, message, args) => {
 let canal = bot.channels.get("702674835343409193");
         
 let error = args.join(' ');
-if(!error) return message.channel.send("Ingrese el error a reportar!\nUsar de manera correcta este comando, de no ser así serás añadido a la blacklist!")
-if(error.length <)   
+if(!error) return message.channel.send(emoji.incorrecto + `**${message.author.username},** Ingrese el error a reportar!\n:rotating_light: Usar de manera correcta este comando, de no ser así serás añadido a la **blacklist**!`)
+if(error.length < 4) return message.channel.send(emoji.incorrectoGif + `**${message.author.username},** El mensaje debe contener al menos 4 carácteres.`)  
    
     
     const embed = new Discord.RichEmbed()
@@ -17,12 +17,12 @@ if(error.length <)
     .addField("**▸ Error:**", error)
     .addField("**▸ Info:**", `»__Usuario:__ ${message.author.tag}\n»__ID usuario:__ ${message.author.id}\n»__Servidor:__ ${message.guild.name}`)
     .setTimestamp()
-    .setFooter("✅ Error reparado. | ❌ No reparado.")
+    .setFooter("✅ Error reparado. | ❌ No reparado. | ❓ Dudoso.")
     message.channel.send(":white_check_mark: | Error enviado, gracias por reportarlo!")
     let msg = await canal.send(embed)
     await msg.react("✅");
     await msg.react("❌");
-  
+    await msg.react("❓");
 }
 module.exports.help = {
   name: "error",
--- commands/server.js
+++ commands/server.js
@@ -1,12 +1,12 @@
 const Discord = require("discord.js");
-
+const emoji = require("../emojis.json");
 module.exports.run = async(bot, message, args) => {
   
   let servidor = message.guild;
   
   const emojisnormales = message.guild.emojis.filter(e => e.animated === false).size;
   const emojisNitro = message.guild.emojis.filter(e => e.animated === true).size;
-  const afkCanal = servidor.afkChannel ? servidor.afkChannel : "<:red_tick:642577040872308766> El servidor no tiene canal de afk.";
+  const afkCanal = servidor.afkChannel ? servidor.afkChannel : `${emoji.incorrecto} El servidor **no** tiene canal de afk.`;
    let texto = message.guild.channels.filter(x => x.type === "text").size;
    let voz = message.guild.channels.filter(x => x.type === "voice").size;
    let categoria = message.guild.channels.filter(x => x.type === "category").size;
@@ -21,14 +21,14 @@ module.exports.run = async(bot, message, args) => {
   
   
   const embed = new Discord.RichEmbed()
-  .setTitle(`<:correcto:558845268800307229> Información sobre ${message.guild.name}.`)
+  .setTitle(`${emoji.correcto} Información sobre ${message.guild.name}.`)
   .addField("• Owner:", `${servidor.owner} \`-\` ${servidor.owner.id} \`-\` #${servidor.owner.user.discriminator} `)
   .addField("• Servidor:", `» Nombre: ${servidor.name}   » Creado: \`${servidor.createdAt.toDateString()}\`\n» Seguridad: ${verificaciones[servidor.verificationLevel]}   » Miembros: ${servidor.memberCount} || **Humanos:** ${message.guild.memberCount - message.guild.members.filter(m => m.user.bot).size} **Bots:** ${message.guild.members.filter(m => m.user.bot).size}`)
   .addField("• Emojis:", `» Normales: \`${emojisnormales}\`   » Nitro: \`${emojisNitro}\` `)
   .addField("• Canal AFK:", `${afkCanal}`)
   .addField("• Canales:", `» Texto: \`${texto}\`    » Voz: \`${voz}\`     » Categoría: \`${categoria}\` `)
   .setColor("#0000FF")
-  .setFooter(`- Solicitado por ${message.author.username}.`)
+  .setFooter(`- Solicitado por ${message.author.username}.`, message.author.avatarURL)
   
   message.channel.send(embed)
 }
