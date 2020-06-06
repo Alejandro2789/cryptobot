1898e93aa32f47b24cbd7d5eb9c536298fe8ec7a
Glitch (material-defiant-foxtrot) <none>
2020-06-06 03:07:18 +0000

    🐁😘 Checkpoint ./commands/plantilla_comando.js:1905663/44 ./commands/pala.js:1905663/251

---
+++ commands/reglas.js
@@ -0,0 +1,16 @@
+const Discord = require("discord.js");
+const emoji = require("../emojis.json");
+
+module.exports.run = async (bot, message, args) => {
+
+const embed = new Discord.RichEmbed()
+.setTitle(emoji.verificado + " Reglas · Crypto Support")
+.setDescription("**1.** Respetar a todos los usuarios del servidor. No se permite ningún tipo de racismo.\n**2.** Utilizar de manera correcta cada canal. Todos los canales tienen su respectivo uso, de no darle un buen uso recibirá una advertencia.\n**3.** No generar discusiones en el servidor, problemas entre usuarios llevarlos a privado.\n**4.** No se permiten multicuentas en el servidor.\n**5.** El [spam](https://es.m.wikipedia.org/wiki/Spam) y [flood](http://www.gamerdic.es/termino/flood/) no están permitidos en el servidor.\n**6.** No se permite cualquier tipo de publicidad de ningún otro tipo de servidor o bot no asociado a **Crypto.**\n**7.** No pidas roles o rangos qué no se te han otorgado. ")
+.setFooter("Crypto Bot", "https://cdn.discordapp.com/attachments/600655797403385875/689130817896972291/CryptNewNew.png")
+
+message.channel.send(embed);
+}
+module.exports.help = {
+  name: "reglas",
+  aliases: []
+}
